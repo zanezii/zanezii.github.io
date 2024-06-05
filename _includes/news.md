3c8fa82827a525266f08f92cab0a7ad517d7eecf
zanezii <925998740@qq.com>
2024-06-05 10:03:22 +0000

    fix show more/less bug

--- _includes/news.md
+++ _includes/news.md
@@ -2,23 +2,21 @@
 
 <h2 style="margin: 60px 0px 10px;">News</h2>
 
-<ul>
+<ul id="newsList">
   {% assign max_visible = 3 %}<!--设置visible item number -->
   {% for link in site.data.news.main %}
   {% if forloop.index <= max_visible %}
   <li><strong>[{{ link.date }}]</strong> {{ link.msg }}</li>
   {% endif %}
   {% endfor %}
-  <li><a href="javascript:toggle_vis('newsmore')">Show More</a></li>
-</ul>
+  <li id="toggleLink"><a href="javascript:toggle_vis('newsmore')">Show More</a></li>
 
-<div id="newsmore" style="display:none">
-  <ul>
+  <div id="newsmore" style="display:none">
     {% for link in site.data.news.main %}
     {% if forloop.index0 >= max_visible %}
     <li><strong>[{{ link.date }}]</strong> {{ link.msg }}</li>
     {% endif %}
     {% endfor %}
-    <li><a href="javascript:toggle_vis('newsmore')">Show Less</a></li>
-  </ul>
-</div>
+  </div>  
+</ul>
+
--- _site/assets/js/showmore.js
+++ _site/assets/js/showmore.js
@@ -1,15 +1,24 @@
-// show more/less点击切换
 function toggle_vis(id) {
-    var element = document.getElementById(id);
-    var linkText = element.style.display === "none" ? "Show Less" : "Show More";
-    var linkElement = document.querySelector(`a[href="javascript:toggle_vis('${id}')"]`);
-    
-    element.style.display = element.style.display === "none" ? "block" : "none";
-    linkElement.textContent = linkText;
-    
-    if (linkText === "Show Less") {
-      linkElement.setAttribute("href", "javascript:toggle_vis('newsmore')");
-    } else {
-      linkElement.setAttribute("href", "javascript:toggle_vis('newsmore')");
-    }
+  var element = document.getElementById(id);
+  var linkElement = document.querySelector(`a[href="javascript:toggle_vis('${id}')"]`);
+  var newsList = document.getElementById("newsList");
+
+  if (element.style.display === "none") {
+      element.style.display = "block";
+      linkElement.textContent = "Show Less";
+
+      // 移动 "Show Less" 链接到最末尾
+      var toggleLink = document.getElementById("toggleLink");
+      newsList.appendChild(toggleLink);
+  } else {
+      element.style.display = "none";
+      linkElement.textContent = "Show More";
+
+      // 移动 "Show More" 链接到之前的位置
+      var toggleLink = document.getElementById("toggleLink");
+      newsList.insertBefore(toggleLink, element);
   }
+
+  // 更新 href 链接
+  linkElement.setAttribute("href", `javascript:toggle_vis('${id}')`);
+}
